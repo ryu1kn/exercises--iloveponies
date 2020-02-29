37fb182fe111ac7457527397b773fcd6d3b77ebe
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-02-29 17:17:41 +1100

    Check-in predicates exercise

---
+++ predicates/project.clj
@@ -0,0 +1,4 @@
+(defproject predicates "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [iloveponies.tests/predicates "0.2.0-SNAPSHOT"]]
+  :profiles {:dev {:plugins [[lein-midje "3.2.1"]]}})
